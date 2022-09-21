8ade31a582a507ddb48b6becaf0f988a70f294dd
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-09-21 17:57:30 +0200

    Add contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,11 @@
-const contact = () => {
+import React, { useState } from "react";
+
+const Contact = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <div className="h-screen">
       <div className="mx-auto rounded-xl w-5/6 h-2/5 bg-beige">
@@ -8,20 +15,28 @@ const contact = () => {
           nous appeler!
         </p>
       </div>
+
       <div className="max-w-2xl mx-auto -mt-40">
-        <form className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500">
+        <form
+        //   onSubmit={handleSubmit}
+          className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500"
+        >
           <h1 className="text-2xl font-bold dark:text-gray-50">Nous écrire</h1>
 
           <label
             htmlFor="fullname"
             className="text-gray-500 font-light mt-8 dark:text-gray-50"
           >
-            Nom et Prénom{" "}
+            Nom et Prénom
             <span className="text-red-500 dark:text-gray-50">*</span>
           </label>
           <input
             type="text"
             name="fullname"
+            value={fullname}
+            onChange={(e) => {
+              setFullname(e.target.value);
+            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
 
@@ -34,6 +49,10 @@ const contact = () => {
           <input
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
 
@@ -46,6 +65,10 @@ const contact = () => {
           <input
             type="text"
             name="subject"
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
 
@@ -57,10 +80,17 @@ const contact = () => {
           </label>
           <textarea
             name="message"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           ></textarea>
           <div className="flex flex-row items-center justify-start">
-            <button className="px-10 mt-8 py-2 bg-[#130F49] text-white font-light rounded-md text-lg flex flex-row items-center">
+            <button
+              type="submit"
+              className="px-10 mt-8 py-2 bg-[#130F49] text-white font-light rounded-md text-lg flex flex-row items-center"
+            >
               Envoyer
               <svg
                 width="24"
@@ -83,4 +113,4 @@ const contact = () => {
   );
 };
 
-export default contact;
+export default Contact;
