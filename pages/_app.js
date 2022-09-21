5fa4bf0a262fa00af034fafae9cd09da35c81ba4
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-09-21 11:30:41 +0200

    Add meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
+import Seo from "./Seo";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, canonical }) {
   return (
     <Layout>
+      <Seo canonical={canonical} />
       <Component {...pageProps} />
     </Layout>
   );
