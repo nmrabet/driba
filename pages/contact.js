120e99e44d093e8bc5704af50a212ac1ce1a99b2
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-09-22 12:21:16 +0200

    Add contact form send message

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_r5tspbo",
+        "template_yphrtpm",
+        form.current,
+        "oI6VDpQZDciv06xxl"
+      )
+      .then(
+        (result) => {
+          alert(result.text);
+        },
+        (error) => {
+          alert(error.text);
+        }
+      );
+  };
 
   return (
     <div className="h-screen">
@@ -18,13 +36,14 @@ const Contact = () => {
 
       <div className="max-w-2xl mx-auto -mt-40">
         <form
-        //   onSubmit={handleSubmit}
+          ref={form}
+          onSubmit={sendEmail}
           className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500"
         >
           <h1 className="text-2xl font-bold dark:text-gray-50">Nous écrire</h1>
 
           <label
-            htmlFor="fullname"
+            htmlFor="name"
             className="text-gray-500 font-light mt-8 dark:text-gray-50"
           >
             Nom et Prénom
@@ -32,11 +51,7 @@ const Contact = () => {
           </label>
           <input
             type="text"
-            name="fullname"
-            value={fullname}
-            onChange={(e) => {
-              setFullname(e.target.value);
-            }}
+            name="user_name"
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
 
@@ -48,11 +63,7 @@ const Contact = () => {
           </label>
           <input
             type="email"
-            name="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            name="user_email"
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
 
@@ -65,10 +76,6 @@ const Contact = () => {
           <input
             type="text"
             name="subject"
-            value={subject}
-            onChange={(e) => {
-              setSubject(e.target.value);
-            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
 
@@ -80,15 +87,12 @@ const Contact = () => {
           </label>
           <textarea
             name="message"
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           ></textarea>
           <div className="flex flex-row items-center justify-start">
             <button
               type="submit"
+              value="Send"
               className="px-10 mt-8 py-2 bg-[#130F49] text-white font-light rounded-md text-lg flex flex-row items-center"
             >
               Envoyer
