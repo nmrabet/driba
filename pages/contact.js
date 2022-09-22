5fa65b35882bac62fe039515115ca18cb3231cf3
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-09-22 14:00:59 +0200

    Add map

--- pages/contact.js
+++ pages/contact.js
@@ -26,98 +26,122 @@ const Contact = () => {
   };
 
   return (
-    <div className="h-screen">
-      <div className="mx-auto rounded-xl w-5/6 h-2/5 bg-beige">
-        <h3 className="text-3xl text-center py-4">Nous contacter</h3>
-        <p className="mx-auto text-center w-1/2">
-          Pour toutes demandes, n&apos;hésitez pas à nous envoyer un message ou
-          nous appeler!
-        </p>
-      </div>
+    <>
+      <div className="h-screen">
+        <div className="mx-auto rounded-xl w-5/6 h-2/5 bg-beige">
+          <h3 className="text-3xl text-center py-4">Nous contacter</h3>
+          <p className="mx-auto text-center w-1/2">
+            Pour toutes demandes, n&apos;hésitez pas à nous envoyer un message
+            ou nous appeler!
+          </p>
+        </div>
 
-      <div className="max-w-2xl mx-auto -mt-40">
-        <form
-          ref={form}
-          onSubmit={sendEmail}
-          className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500"
-        >
-          <h1 className="text-2xl font-bold dark:text-gray-50">Nous écrire</h1>
-
-          <label
-            htmlFor="name"
-            className="text-gray-500 font-light mt-8 dark:text-gray-50"
+        <div className="max-w-2xl mx-auto -mt-40">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500"
           >
-            Nom et Prénom
-            <span className="text-red-500 dark:text-gray-50">*</span>
-          </label>
-          <input
-            type="text"
-            name="user_name"
-            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
-            required
-          />
+            <h1 className="text-2xl font-bold dark:text-gray-50">
+              Nous écrire
+            </h1>
 
-          <label
-            htmlFor="email"
-            className="text-gray-500 font-light mt-4 dark:text-gray-50"
-          >
-            E-mail<span className="text-red-500">*</span>
-          </label>
-          <input
-            type="email"
-            name="user_email"
-            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
-            required
-          />
-
-          <label
-            htmlFor="subject"
-            className="text-gray-500 font-light mt-4 dark:text-gray-50"
-          >
-            Objet<span className="text-red-500">*</span>
-          </label>
-          <input
-            type="text"
-            name="subject"
-            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
-          />
-
-          <label
-            htmlFor="message"
-            className="text-gray-500 font-light mt-4 dark:text-gray-50"
-          >
-            Message<span className="text-red-500">*</span>
-          </label>
-          <textarea
-            name="message"
-            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
-            required
-          ></textarea>
-          <div className="flex flex-row items-center justify-start">
-            <button
-              type="submit"
-              value="Send"
-              className="px-10 mt-8 py-2 bg-blue text-white font-light rounded-md text-lg flex flex-row items-center"
+            <label
+              htmlFor="name"
+              className="text-gray-500 font-light mt-8 dark:text-gray-50"
             >
-              Envoyer
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="text-cyan-500 ml-2"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
+              Nom et Prénom
+              <span className="text-red-500 dark:text-gray-50">*</span>
+            </label>
+            <input
+              type="text"
+              name="user_name"
+              className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
+              required
+            />
+
+            <label
+              htmlFor="email"
+              className="text-gray-500 font-light mt-4 dark:text-gray-50"
+            >
+              E-mail<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="email"
+              name="user_email"
+              className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
+              required
+            />
+
+            <label
+              htmlFor="subject"
+              className="text-gray-500 font-light mt-4 dark:text-gray-50"
+            >
+              Objet<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              name="subject"
+              className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
+            />
+
+            <label
+              htmlFor="message"
+              className="text-gray-500 font-light mt-4 dark:text-gray-50"
+            >
+              Message<span className="text-red-500">*</span>
+            </label>
+            <textarea
+              name="message"
+              className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-beige font-light text-gray-500"
+              required
+            ></textarea>
+            <div className="flex flex-row items-center justify-start">
+              <button
+                type="submit"
+                value="Send"
+                className="px-10 mt-8 py-2 bg-blue text-white font-light rounded-md text-lg flex flex-row items-center"
               >
-                <path
-                  d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z"
+                Envoyer
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="text-cyan-500 ml-2"
                   fill="currentColor"
-                />
-              </svg>
-            </button>
-          </div>
-        </form>
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+
+      <div className="mx-auto max-w-4xl flex justify-between my-12">
+        <div>
+          <p className="text-3xl uppercase font-bold">Où nous trouver</p>
+        </div>
+        <div>
+          <p>Adresse</p>
+          <p>Téléphone</p>
+          <p>Horaires</p>
+          <p>Email</p>
+        </div>
+      </div>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6383.168793070823!2d10.2685991!3d36.876365600000014!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2b56f9eca1331%3A0x419bad5d042f60d4!2sAtelier%20Driba!5e0!3m2!1sfr!2ses!4v1663847583176!5m2!1sfr!2ses"
+        width="100%"
+        height="450"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
   );
 };
 
